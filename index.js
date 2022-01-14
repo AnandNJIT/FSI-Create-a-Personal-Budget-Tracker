2789fd8779c2bf6d945dfd86f51c1c9c6d4cb9b0
ANANDNJIT <aplv180@gmail.com>
2022-01-13 19:00:28 -0500

    changed font size on navbar

--- index.js
+++ index.js
@@ -3,3 +3,36 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+window.prompt(weeklyExpenseQuestions[0])
+
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
+
+
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+
+
+
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+
+
+
